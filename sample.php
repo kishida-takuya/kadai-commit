58393be44cc3a39dc53407c241d5b3db9094f2bd
takuya.kishida <kishida-takuya@agex.co.jp>
2022-07-14 08:21:52 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：無し</p>
+        <form acition="sample.php" method="POST">
+            <button type="submit" name="act" valeu="draw">おみくじを引く！</botton>
+        </form>
+    </body>
+</html>
